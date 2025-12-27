efb450481f91125ef36235c6bdbbbe5e91a3b73c
PoluyanbIch <poluyanovia@yandex.ru>
2025-12-27 23:09:33 +0300

    solve 1114

---
+++ 1114.PrintInOrder.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Foo struct {
+	ch1 chan struct{}
+	ch2 chan struct{}
+}
+
+func NewFoo() *Foo {
+	f := &Foo{
+		ch1: make(chan struct{}),
+		ch2: make(chan struct{}),
+	}
+	return f
+}
+
+func (f *Foo) First(printFirst func()) {
+	// Do not change this line
+	printFirst()
+	f.ch1 <- struct{}{}
+}
+
+func (f *Foo) Second(printSecond func()) {
+	/// Do not change this line
+	<-f.ch1
+	printSecond()
+	f.ch2 <- struct{}{}
+}
+
+func (f *Foo) Third(printThird func()) {
+	// Do not change this line
+	<-f.ch2
+	printThird()
+}
+
+func main() {
+	f := NewFoo()
+	go f.Third(func() { fmt.Println("Third") })
+	go f.First(func() { fmt.Println("First") })
+	go f.Second(func() { fmt.Println("Second") })
+	time.Sleep(time.Second)
+}
