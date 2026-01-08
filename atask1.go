2c4de27141a0ef626bc216d77efe91a5addab4c6
PoluyanbIch <poluyanovia@yandex.ru>
2026-01-08 16:20:00 +0300

    atask1.go

---
+++ atask1.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+func main() {
+	var L, W, A, B int64
+	if _, err := fmt.Scan(&L, &W); err != nil {
+		return
+	}
+	if _, err := fmt.Scan(&A, &B); err != nil {
+		return
+	}
+	ans := (L / A) * (W / B)
+
+	fmt.Println(ans)
+}
+*/
